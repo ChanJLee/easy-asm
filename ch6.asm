aaa1318e36986825f51714fdf6faa7488f5ea854
ChanJLee <1355425625@qq.com>
2018-03-06 21:56:49 +0800

    sync

--- ch6.asm
+++ ch6.asm
@@ -1,10 +1,21 @@
 assume cs: codeseg
 
 codeseg segment
-    mov ax, 2
-    mov cx, 10
-    s: add ax, ax
+    mov ax, 1000H
+    mov ds, ax
+
+    mov cx, 3H
+    mov bx, 0H
+
+    s:  mov ds:[bx], bx
+        inc bx
         loop s
+
+    a:  mov al, ds:[bx]
+        mov ah, 0H
+        add dx, ax
+        inc bx
+        loop a
     
     mov ax, 4200h
     int 21h
