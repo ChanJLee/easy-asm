d289cb5e88e41bb07f8444ec70de5b84645c1a7d
ChanJLee <1355425625@qq.com>
2018-03-27 23:20:45 +0800

    add-asm-func

---
+++ ch10_2.asm
@@ -0,0 +1,36 @@
+assume cs:code
+
+stack segment
+    dw 1, 2, 3, 4, 5, 6, 7, 8
+    dd 0, 0, 0, 0, 0, 0, 0, 0
+stack ends
+
+code segment
+    start:
+        mov ax, stack
+        mov ds, ax
+
+        mov si, 0
+        mov di, 16
+
+        mov cx, 3
+
+        s: 
+            mov bx, ds:[si]
+            call f
+            mov ds:[di], ax
+            mov ds:[di + 2], dx
+            add si, 2
+            add di, 4
+            loop s
+
+        f:
+            mov ax, bx
+            ml bx
+            ml bx
+            ret
+
+        mov ax, 4200h
+        int 21h
+code ends
+end start
